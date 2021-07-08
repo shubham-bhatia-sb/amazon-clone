d62d270048e2ae90c3bd787976d49f5ef3125777
webcts <86883509+webcts@users.noreply.github.com>
2021-07-08 19:10:03 +0530

    Create product.js

---
+++ components/product.js
@@ -0,0 +1,32 @@
+import styles from './Product.module.css';
+;
+function Product({ id, title, image, price, rating }) {
+ 
+    return (
+      <div className={styles.display}>
+      <div className={styles.product}>
+        <div className={styles.product__info}>
+          <p>{title}</p>
+          <p className={styles.product__price}>
+            <small>$</small>
+            <strong>{price}</strong>
+          </p>
+          <div className={styles.product__rating}>
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>🌟</p>
+              ))}
+          </div>
+        </div>
+  
+        <img src={image} alt="" />
+  
+        <button>Add to Basket</button>
+      </div>
+      </div>
+    );
+  }
+  
+  export default Product;
+  
