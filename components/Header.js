db122020225bbaca4b16d4d4be332d3ffa446bf3
webcts <86883509+webcts@users.noreply.github.com>
2021-07-08 19:08:00 +0530

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,52 @@
+import  beauty from "./Header.module.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {  
+  return (
+    <div className={beauty.header}>
+      
+        <img
+          className={beauty.header__logo}
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      
+
+      <div className={beauty.header__search}>
+        <input className={beauty.header__searchInput} type="text" />
+        <SearchIcon className={beauty.header__searchIcon} />
+      </div>
+
+      <div className={beauty.header__nav}>
+        
+          <div className={beauty.header__option}>
+            <span className={beauty.header__optionLineOne}>Hello Guest</span>
+            <span className={beauty.header__optionLineTwo}>Sign In</span>
+          </div>
+        
+
+        
+          <div className={beauty.header__option}>
+            <span className={beauty.header__optionLineOne}>Returns</span>
+            <span className={beauty.header__optionLineTwo}>& Orders</span>
+          </div>
+        
+        
+
+        <div className={beauty.header__option}>
+          <span className={beauty.header__optionLineOne}>Your</span>
+          <span className={beauty.header__optionLineTwo}>Prime</span>
+        </div>
+
+        
+          <div className={beauty.header__optionBasket}>
+            <ShoppingBasketIcon />
+            <span className={beauty.header__basketCount}>0</span>
+          </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Header;
